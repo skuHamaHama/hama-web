9148c678288d6c47656d31f342d72a13e67249fa
yujin <uj0791navercom@skuniv.ac.kr>
2023-07-29 12:05:56 +0900

    라우터 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as Screen from "./screens/index";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Screen.Login />,
+    },
+    {
+      path: "/join",
+      element: <Screen.Join />,
+    },
+  ]);
 
+  return <RouterProvider router={router} />;
+}
 export default App;
