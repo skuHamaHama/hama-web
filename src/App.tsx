20e6ae9dd39f5f07dd8b7148ed4a7b9f2b53480b
yujin <uj0791navercom@skuniv.ac.kr>
2023-08-07 00:22:36 +0900

    feat: 글꼴 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import * as Screen from "./screens/index";
-// import "./App.css";
+import * as auth from "./hooks/screens/auth/index";
+import * as main from "./hooks/screens/main/index";
 
-function App() {
+export function App() {
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Screen.Intro />,
-    },
-    {
-      path: "/login",
-      element: <Screen.Login />,
-    },
-    {
-      path: "/join",
-      element: <Screen.Join />,
-    },
+    { path: "/", element: <main.Intro /> },
+    { path: "/login", element: <auth.Login /> },
+    { path: "/join", element: <auth.Join /> },
   ]);
 
   return <RouterProvider router={router} />;
 }
-
-export default App;
