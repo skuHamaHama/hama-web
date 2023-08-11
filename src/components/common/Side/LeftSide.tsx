45a334050a01027e7f10634cf9311914138a967a
xlzbthxyjkoo <happyyj01@naver.com>
2023-08-11 15:20:24 +0900

    feat: left sidebar

--- src/components/common/Side/LeftSide.tsx
+++ src/components/common/Side/LeftSide.tsx
@@ -1,7 +1,66 @@
+import React from "react";
 import * as S from "./LeftSide.styled";
 
+// const items= [
+//   {
+//     type: 'nickname',
+//     title: '닉네임 변경',
+//   },
+//   {
+//     type: 'password',
+//     title: '비밀번호 변경',
+//   },
+//   {
+//     type: 'nickname',
+//     title: '닉네임 변경',
+//   },
+
+// ]
+
 function LeftSide() {
-  return <S.Container></S.Container>;
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const onToggle = () => setIsOpen(!isOpen);
+  const onOptionClicked = (value: string, index: number) => () => {
+    console.log(value);
+    console.log(index);
+    setIsOpen(false);
+  };
+
+  return (
+    <S.Container>
+      <S.Info onClick={onToggle}>
+        <p>내 정보</p>
+      </S.Info>
+      <S.DropDownContainer>
+        {isOpen && (
+          <>
+            <S.Line />
+            <S.ListItem onClick={onOptionClicked("닉네임 변경", 1)}>
+              닉네임 변경
+            </S.ListItem>
+            <S.ListItem onClick={onOptionClicked("비밀번호 변경", 2)}>
+              비밀번호 변경
+            </S.ListItem>
+            <S.ListItem onClick={onOptionClicked("회원탈퇴", 3)}>
+              회원탈퇴
+            </S.ListItem>
+          </>
+        )}
+      </S.DropDownContainer>
+      <S.CategoryMenuBox>
+        <p>최근 본 쿠폰</p>
+      </S.CategoryMenuBox>
+      <S.CategoryMenuBox>
+        <p>사용한 쿠폰</p>
+      </S.CategoryMenuBox>
+      <S.CategoryMenuBox>
+        <p>찜한 쿠폰</p>
+      </S.CategoryMenuBox>
+      <S.CategoryMenuBox>
+        <p>작성한 후기</p>
+      </S.CategoryMenuBox>
+    </S.Container>
+  );
 }
 
 export default LeftSide;
