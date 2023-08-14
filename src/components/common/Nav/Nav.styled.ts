e48e835d8956de68df083d801a1b2261ff8ef279
xlzbthxyjkoo <happyyj01@naver.com>
2023-08-15 03:47:25 +0900

    feat: nav

--- src/components/common/Nav/Nav.styled.ts
+++ src/components/common/Nav/Nav.styled.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Line = styled.div`
   border: 1px solid #3c87bd;
@@ -14,8 +14,100 @@ export const Container = styled.div`
   }
 `;
 
-export const Food = styled.div``;
+export const Food = styled.div`
+  cursor: pointer;
+`;
 
-export const Culture = styled.div``;
+export const Culture = styled.div`
+  cursor: pointer;
+`;
 
-export const Shopping = styled.div``;
+export const Shopping = styled.div`
+  cursor: pointer;
+`;
+
+export const Wrapper = styled.div`
+  margin: 100px auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+  font-size: 19px;
+  background: gray;
+  width: 400px;
+  height: 50px;
+  font-weight: bold;
+`;
+
+export const DropdownContainer = styled.div`
+  position: relative;
+  text-align: center;
+`;
+
+export const DropdownButton = styled.div`
+  cursor: pointer;
+`;
+
+export const Menu = styled.div`
+  background: gray;
+  position: absolute;
+  top: 52px;
+  left: 50%;
+  width: 100px;
+  text-align: center;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  opacity: 0;
+  visibility: hidden;
+  transform: translate(-50%, -20px);
+  transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+  z-index: 9;
+
+  &:after {
+    content: "";
+    height: 0;
+    width: 0;
+    position: absolute;
+    top: -3px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 12px solid transparent;
+    border-top-width: 0;
+    border-bottom-color: gray;
+  }
+
+  ${({ isDropped }) =>
+    isDropped &&
+    css`
+      opacity: 1;
+      visibility: visible;
+      transform: translate(-50%, 0);
+      left: 50%;
+    `};
+`;
+
+export const Ul = styled.ul`
+  & > li {
+    margin-bottom: 10px;
+  }
+
+  & > li:first-of-type {
+    margin-top: 10px;
+  }
+
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Li = styled.li``;
+
+export const LinkWrapper = styled.a`
+  font-size: 16px;
+  text-decoration: none;
+  color: white;
+`;
--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -1,18 +1,68 @@
+import React from "react";
 import * as S from "./Nav.styled";
+import useDetectClose from "./useDetectClose";
 
 export const Nav = () => {
+  const [FoodIsOpen, FoodRef, FoodHandler] = useDetectClose(false);
+  const [CultureIsOpen, CultureRef, CultureHandler] = useDetectClose(false);
+  const [ShopIsOpen, ShopRef, ShopHandler] = useDetectClose(false);
+
   return (
     <>
       <S.Container>
-        <S.Food>
-          <p>음식</p>
-        </S.Food>
-        <S.Culture>
-          <p>문화생활</p>
-        </S.Culture>
-        <S.Shopping>
-          <p>쇼핑</p>
-        </S.Shopping>
+        <S.DropdownContainer>
+          <S.DropdownButton onClick={FoodHandler} ref={FoodRef}>
+            음식
+          </S.DropdownButton>
+          <S.Menu isDropped={FoodIsOpen}>
+            <S.Ul>
+              <S.Li>
+                <S.LinkWrapper href="#1-1">식당</S.LinkWrapper>
+              </S.Li>
+              <S.Li>
+                <S.LinkWrapper href="#1-2">카페</S.LinkWrapper>
+              </S.Li>
+            </S.Ul>
+          </S.Menu>
+        </S.DropdownContainer>
+
+        <S.DropdownContainer>
+          <S.DropdownButton onClick={CultureHandler} ref={CultureRef}>
+            문화생활
+          </S.DropdownButton>
+          <S.Menu isDropped={CultureIsOpen}>
+            <S.Ul>
+              <S.Li>
+                <S.LinkWrapper href="#2-1">영화</S.LinkWrapper>
+              </S.Li>
+              <S.Li>
+                <S.LinkWrapper href="#2-2">놀이공원</S.LinkWrapper>
+              </S.Li>
+              <S.Li>
+                <S.LinkWrapper href="#2-3">게임</S.LinkWrapper>
+              </S.Li>
+            </S.Ul>
+          </S.Menu>
+        </S.DropdownContainer>
+
+        <S.DropdownContainer>
+          <S.DropdownButton onClick={ShopHandler} ref={ShopRef}>
+            쇼핑
+          </S.DropdownButton>
+          <S.Menu isDropped={ShopIsOpen}>
+            <S.Ul>
+              <S.Li>
+                <S.LinkWrapper href="#3-1">옷</S.LinkWrapper>
+              </S.Li>
+              <S.Li>
+                <S.LinkWrapper href="#3-2">신발</S.LinkWrapper>
+              </S.Li>
+              <S.Li>
+                <S.LinkWrapper href="#3-3">화장품</S.LinkWrapper>
+              </S.Li>
+            </S.Ul>
+          </S.Menu>
+        </S.DropdownContainer>
       </S.Container>
       <S.Line />
     </>
