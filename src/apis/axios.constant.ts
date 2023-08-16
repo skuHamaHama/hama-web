e60a79e65eecd3e5f7000a944f4b952fbb3db8c3
yujin <uj0791navercom@skuniv.ac.kr>
2023-08-16 21:41:25 +0900

    fix: url

--- src/apis/axios.constant.ts
+++ src/apis/axios.constant.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://43.202.132.83:8088/api";
+export const BASE_URL = "http://43.202.132.83:8088/api";
--- src/setProxy.ts
+++ src/setProxy.ts
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app: Express) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://43.202.132.83:8088",
+      target: "http://43.202.132.83:8088",
       changeOrigin: true,
     })
   );
